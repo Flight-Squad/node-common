17123af483ba90d2d494e4e77da6893e2baa0bf7
Pujit Mehrotra <pujitm2009@gmail.com>
2020-02-19 20:15:25 +0000

    Fix path resolution bug in chatsquad.ts

--- src/flightsquad/chatsquad.ts
+++ src/flightsquad/chatsquad.ts
@@ -4,40 +4,46 @@ import { ExtendRouter } from "../router";
 import { Trip, TripGroupQuery } from "./trip";
 
 export interface PaymentMessageReq {
-    /** messaging platform */
-    platform: string;
-    /** user id */
-    id: string;
-    payment: CreateTransactionResponse;
-    trip: Trip;
-    query: TripGroupQuery;
+  /** messaging platform */
+  platform: string;
+  /** user id */
+  id: string;
+  payment: CreateTransactionResponse;
+  trip: Trip;
+  query: TripGroupQuery;
 }
 
 export interface SendMessageReq {
-    /** messaging platform */
-    platform: string;
-    /** user id */
-    id: string;
-    /** message to send */
-    message: string;
+  /** messaging platform */
+  platform: string;
+  /** user id */
+  id: string;
+  /** message to send */
+  message: string;
 }
 
 export class Chatsquad {
-    constructor(readonly apiRoot: string) {}
-    private static readonly routers = {
-        send: {
-            messages: ExtendRouter('/send/messages')
-        }
+  constructor(readonly apiRoot: string) {}
+  private static readonly routers = {
+    send: {
+      messages: ExtendRouter("/send/messages")
     }
-    static readonly routes = {
-        messages: {
-            msg: Chatsquad.routers.send.messages('/'),
-            payment: Chatsquad.routers.send.messages('/payment'),
-        }
+  };
+  static readonly routes = {
+    messages: {
+      msg: Chatsquad.routers.send.messages("/"),
+      payment: Chatsquad.routers.send.messages("/payment")
     }
-    
-    send = {
-        msg: (req: SendMessageReq) => Axios.post(`${this.apiRoot}/${Chatsquad.routes.messages.msg}`, req),
-        payment: async (req: PaymentMessageReq): Promise<void> => {await Axios.post(`${this.apiRoot}/${Chatsquad.routes.messages.payment}`, req)}
+  };
+
+  send = {
+    msg: (req: SendMessageReq) =>
+      Axios.post(`${this.apiRoot}${Chatsquad.routes.messages.msg}`, req),
+    payment: async (req: PaymentMessageReq): Promise<void> => {
+      await Axios.post(
+        `${this.apiRoot}${Chatsquad.routes.messages.payment}`,
+        req
+      );
     }
+  };
 }
